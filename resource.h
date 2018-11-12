3dfef6e8d5d82c3f90135d6cdd5d15df5e354c86
sungmi-mPLUS <42432207+sungmi-mPLUS@users.noreply.github.com>
2018-11-12 18:36:12 +0900

    first end

---
+++ resource.h
@@ -0,0 +1,48 @@
+��//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// FTPClient.rc에서 사용되고 있습니다.
+//
+#define IDD_FTP_CLIENT_DIALOG           102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     131
+#define IDD_CONNECT_DIALOG              131
+#define IDD_DIALOG_UPLOAD               134
+#define IDC_MFCBUTTON1                  1000
+#define IDC_LIST_LDIR                   1001
+#define IDC_LIST_LFILE                  1002
+#define IDC_LIST_RDIR                   1003
+#define IDC_LIST_RFILE                  1004
+#define IDC_DOWN_BTN                    1005
+#define IDC_UPLOAD_BTN                  1006
+#define IDC_LOCAL_TEXT                  1007
+#define IDC_LOCALDIRTEXT                1008
+#define IDC_REMOTEDIRTEXT               1009
+#define IDC_IPADDRESS1                  1010
+#define IDC_IP_ADDRESS                  1010
+#define IDC_EDIT1                       1011
+#define IDC_EDIT2                       1012
+#define IDC_NAME                        1012
+#define IDC_CONNECT_INFO                1012
+#define IDC_EDIT3                       1013
+#define IDC_PASSWORD                    1013
+#define IDC_LIST_THREAD                 1013
+#define IDC_MFCBUTTON_THREAD            1014
+#define IDC_BTN_BACKUPFOLDER            1015
+#define IDC_MFCBUTTON_THREAD2           1015
+#define IDC_STATIC_BACKUP_F             1016
+#define IDC_RADIO1                      1017
+#define IDC_RADIO2                      1018
+#define IDC_RADIO3                      1019
+#define IDC_RADIO4                      1020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
